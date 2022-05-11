1304c50b4266e5a559ad34e003cd53a1c106bec3
swerts <odante.swerts@hotmail.com>
2022-05-11 15:26:21 -0300

    closures lesson

--- exercise/closures_iterators/src/main.rs
+++ exercise/closures_iterators/src/main.rs
@@ -2,23 +2,55 @@
 #[allow(unused_mut)]
 
 fn main() {
+
+    // let s = "🍓";
+
+    // let f = move || {
+    //     println!("{}", s);
+    // };
+
+    // let f = ||{
+    //     println!("{}", s);
+    // };
+
+    // f();
+
+    // println!("{}", s);
+
+    println!("#######################################");
+    println!("## EX 1");
+    println!("#######################################");
+
     // 1. Uncomment the code below. Create a closure that returns the square of an integer (the
     // number multiplied by itself), and assign the closure to the "square" variable. Then run the
     // code and make sure it works.
 
-    // let square = ...
-    // println!("5 squared is {}", square(5));
+    let square = |x: i32| x.pow(2);
+    println!("5 squared is {}", square(5));
+
+    println!();
+    println!("#######################################");
+    println!("## EX 2");
+    println!("#######################################");
 
     // 2. Uncomment the code below.  Finish the .map() iterator adaptor call by passing it a closure
     // which takes a tuple of two integers as a parameter, and returns a tuple with the first
     // integer incremented by 1, and the second integer left alone.  For example, if given the input
     // (0, 1), it should return (1, 1). Run the code and make sure it works.
 
-    // let pairs = vec![(0, 1), (2, 3), (4, 5)];
-    // pairs
-    //     .into_iter()
-    //     .map( ... )
-    //     .for_each(|t| println!("{:?}", t));
+    let pairs = vec![(0, 1), (2, 3), (4, 5)];
+    
+    pairs
+        .into_iter()
+        .map(|(mut x,y)| (x + 1  ,y))
+        // .map(|t| (t.0 + 1, t.1))
+        .for_each(|y| println!("{:?}", y));
+
+    
+    println!();
+    println!("#######################################");
+    println!("## EX 3");
+    println!("#######################################");
 
     // 3. Uncomment the code below. There is a mutable vector named `numbers`. Use an iterator over
     // mutable references to multiply each of the values in `numbers` by 3.
@@ -31,21 +63,54 @@ fn main() {
     // }
     // println!("{:?}", numbers); // should print [3, 6, 9, 12]
 
+    let mut numbers = vec![1, 2, 3, 4];
+
+    numbers.iter_mut()
+        .map(|x| *x * 3)
+        .for_each(|x| println!("{}", x));
+    
+    println!("");
+
+    numbers.iter().for_each(|x| print!("{},", x));
+
+
+    // for x in &mut numbers {
+    //     let bauau = *x * 3;
+    // }
+
+    println!();
+    println!("#######################################");
+    println!("## EX 4");
+    println!("#######################################");
+
     // 4. Uncomment the code below.  Take the vector of words and
     // - Convert the vector into an iterator with .into_iter()
     // - Use .filter() to remove any word that contains the letter "h" -- use .contains()
     // - Use .map() to convert all the words to uppercase -- use .to_uppercase()
-    // - Use .collect() to put the transformed words back into a vector
+    // - Use .collect() to put the transformed words back into a vector1
     //
     // Hint: .to_uppercase() is a method on `str` which returns a String
 
-    // let words = vec!["autobot", "beach", "car", "decepticon", "energon", "frothy"];
-    // let transformed...  // do the stuff here
-    // println!("Transformed: {:?}", transformed);
+    let words = vec!["autobot", "beach", "car", "decepticon", "energon", "frothy"];
+    let transformed =  words.into_iter()
+        .filter(|x| !x.contains("h"))
+        .map(|x| x.to_uppercase())
+        .collect::<Vec<_>>();
+    
+    transformed.iter().for_each(|x| println!("{}", x));
 
+    println!("### PRINT DEBUG");
+
+    println!("Transformed: {:?}", transformed);
+
+    println!();
+    println!("#######################################");
+    println!("## EX CHALLENGE");
+    println!("#######################################");
     // Challenge:
     //
     // - Rewrite the code in #2 as a for loop
     // - Rewrite the code in #3 in functional style (without a for loop).  Hint: There are multiple
     // ways to accomplish this, but they all end with an iterator consumer.
 }
+
